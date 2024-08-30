0a181744b000a67e56fb6208177bd64ec963ba31
Vishnu Syamala <vvishnusyamala@gmail.com>
2024-08-30 20:18:03 +0530

    test

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -54,6 +54,7 @@ const Technologies = () => {
 
       <Grid container spacing={3}>
         <Grid item xs={12} md={3}></Grid>
+
         <Grid item xs={12} md={6}>
           <motion.div
             whileInView={{ opacity: 1, x: 0 }}
